9c5dbc4fb3cb69a0d286e0721436911df6f0e11a
Khaleel McDonald <khaleel.mcdonald@gmail.com>
2016-10-12 07:24:46 -0500

    libraries added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+(function(){
+    'use strict';
+
+    angular.module('BillsApp', []).controller('BillsAppController', function($scope) {
+
+    });
+    // containerHeight = Math.ceil(containerHeight / 250.0) * 250;
+})();
