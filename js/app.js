5aaa94e42ae704748f617ab364ef1d7c679b6787
Khaleel McDonald <khaleel.mcdonald@gmail.com>
2016-10-12 07:32:03 -0500

    Hello, World

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
 
-    angular.module('BillsApp', []).controller('BillsAppController', function($scope) {
+    angular.module('BillsApp', ['ngMaterial']).controller('BillsAppController', function($scope) {
 
     });
     // containerHeight = Math.ceil(containerHeight / 250.0) * 250;
