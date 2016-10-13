65d723fc4d1510031937ca2f88598f04b39c38d1
Khaleel McDonald <khaleel.mcdonald@gmail.com>
2016-10-13 16:21:56 -0500

    Adjusted to the look of the final figure display

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
     'use strict';
 
     angular.module('BillsApp', ['ngMaterial']).controller('BillsAppController', function($scope) {
-        // goto('page1');
         $scope.currentNavItem = 'Bill Buffer';
     })
     .controller('BillBufferFormCtrl', function($scope) {
@@ -42,32 +41,30 @@
             water: 0.00
         }
 
+        $scope.subheadClass  = "md-body-1";
+        $scope.titleClass    = "md-subhead";
+
         $scope.displayRent = function() {
-            // $scope.buffer['rent'] = calculateRentBuffer($scope.user['rent']);
             $scope.buffer['rent'] = calculateBuffer($scope.user['rent'], 'rent');
             $scope.total['rent'] = $scope.user['rent'] + $scope.buffer['rent'];
         }
 
         $scope.displayISP = function() {
-            // $scope.buffer['rent'] = calculateRentBuffer($scope.user['rent']);
             $scope.buffer['isp'] = calculateBuffer($scope.user['isp'], 'isp');
             $scope.total['isp'] = $scope.user['isp'] + $scope.buffer['isp'];
         }
 
         $scope.displayPower = function() {
-            // $scope.buffer['power'] = calculatePowerBuffer($scope.user['power']);
             $scope.buffer['power'] = calculateBuffer($scope.user['power'], 'power');
             $scope.total['power'] = $scope.user['power'] + $scope.buffer['power'];
         }
 
         $scope.displayPhone = function() {
-            // $scope.buffer['power'] = calculatePowerBuffer($scope.user['power']);
             $scope.buffer['phone'] = calculateBuffer($scope.user['phone'], 'phone');
             $scope.total['phone'] = $scope.user['phone'] + $scope.buffer['phone'];
         }
 
         $scope.displayWater = function() {
-            // $scope.buffer['power'] = calculatePowerBuffer($scope.user['power']);
             $scope.buffer['water'] = calculateBuffer($scope.user['water'], 'water');
             $scope.total['water'] = $scope.user['water'] + $scope.buffer['water'];
         }
@@ -79,6 +76,10 @@
 
         // The future!
         function calculateBuffer(num, category) {
+            // ensures "NaN" isn't displayed for non-numerical inputs
+            if (isNaN(num) || num == '' || num == null) {
+                return 0;
+            }
             switch (category) {
                 case 'rent':
                     return  (Math.ceil(parseInt(num) / 5000) * 5000) - num;
@@ -101,5 +102,4 @@
         }
 
     });
-    // containerHeight = Math.ceil(containerHeight / 250.0) * 250;
 })();
